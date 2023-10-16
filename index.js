b2c66c7dc1790786cbb9ee7adbb7416db2b16c1b
abdullohasia <tolaganova624@gmail.com>
2023-10-16 20:16:59 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,89 @@
+
+
+Misol_1
+
+func1(1, 2, 3, 4, 5, 6)
+func1( ("aaa"), ("Falastin"), ("O'zbekiston"), 4, 5);
+function func1(a, b, c, d, e) {
+
+    console.log(arguments[0]);
+
+    console.log(arguments[1]);
+     
+    console.log(arguments[2]);
+
+    console.log(arguments[3]);
+
+    console.log(arguments[4]);
+    
+  }
+  
+
+
+// Misol_1
+
+  function func(a, b, c, d) {
+    // arguments[0] = 100; 
+    // arguments[1] = 10; 
+    // arguments[2] = 1; 
+    console.log( arguments[0]+ 1);
+    console.log( arguments[1]+ 10);
+    console.log( arguments[2] + 100 );
+  }
+//   func(("ha")); 
+
+
+
+
+
+// Misol_2
+
+const Abdulloh = function( a, b, d, u, l, l, o, h ) {
+
+    return a + b + d + u + l + l + o + h
+}
+// console.log(Abdulloh( 10, 20, 30, 40, 50, 60, 70, 80 ))
+
+
+const Abdullloh = function( a, b, d, u, l, l, o, h ) {
+
+    return a - b - d - u - l - l - o - h;
+}
+ //console.log(Abdullloh( 10, 20, 30, 40, 50, 60, 70, 80 ))
+
+
+const Abdulllloh = function( a, b, d, u, l, l, o, h ) {
+
+    return a * b - d + u / l + l - o * h
+}
+//console.log(Abdulllloh( 10, 20, 30, 40, 50, 60, 70, 80 ))
+
+
+const Abdullllloh = function( a, b, d, u, l, l, o, h ) {
+
+    return a ** b + d ** u + l ** l + o ^ h
+}
+console.log(Abdullllloh( 10, 20, 30, 40, 50, 60, 70, 80 ))
+
+
+const Abdulllllloh = function( a, b, d, u, l, l, o, h ) {
+
+    return a / b / d / u / l / l / o / h
+}
+//console.log(Abdulllllloh( 10, 20, 30, 40, 50, 60, 70, 80 ))
+
+
+
+
+
+
+let str = "abdulloh"
+
+for ( i = 0; i < 8; i++ ) {
+
+}
+
+
+
+
+
